7145d46f62b75cc4d9e0b120d2dbd7df84dd7999
Zachary Dj <zachary@zacharys-mbp.home>
2023-12-25 22:04:30 -0500

    finalized database setup and fixed bug with database(finally)

--- website/__init__.py
+++ website/__init__.py
@@ -6,23 +6,36 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "secretpassword"
-    app.config['SQL_ALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-    
+
     from .views import views
     from .auth import auth
-    
+
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
-    
-    create_databse(app)
-    
-    
+
+    from .models import User
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    #if someone is not logged in redirect them to login page
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    #allows to access info related to user from db given user id from session(temporary storage)
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
-def create_databse(app):
+def create_database(app):
     if not path.exists("website/" + DB_NAME):
-        db.create_all(app=app)
+        with app.app_context():
+            db.create_all()
--- website/models.py
+++ website/models.py
@@ -1,6 +1,6 @@
-from . import db 
+from website import db
 from flask_login import UserMixin
-from sql_alchemy.sql import func
+from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     #columns for databse
@@ -8,4 +8,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True) #verify for unique emails
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    date_created = db.Column(db.Date(timezone=True), default=func.now())#date of account creation
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())#date of account creation
