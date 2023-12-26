4248f72ffbf35459a831f87fbc9f4fcdf5a66b54
Zachary Dj <zachary@zacharys-mbp.home>
2023-12-25 22:57:55 -0500

    added account verification and adding to databse if valid

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db 
+from .models import User
+
 
 auth = Blueprint("auth", __name__)
 
@@ -10,10 +13,27 @@ def login():
 
 @auth.route("/sign-up", methods=['GET','POST'])
 def sign_up():
-    email = request.form.get("email")
-    username = request.form.get("username")
-    password1= request.form.get("password1")
-    password2 = request.form.get("password2")
+    if request.method == 'POST':
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password1= request.form.get("password1")
+        password2 = request.form.get("password2")
+        
+        #filter database to check is email=email
+        email_exists = User.query.fliter_by(email=email).first() 
+        username_exists = User.query.filter_by(username=username).first()
+        if email_exists:
+            flash("Email is already in use", category='error')
+        elif username_exists:
+            flash("Username is already in use", category='error')
+        elif password1 != password2:
+            flash("Passwords dont't match", category='error')
+        elif len(username) < 2:
+            flash("Username must be at least 3 characters long", category='error')
+        elif len(password1) < 6:
+            flash("Password must be at least 6 characters long", category='error')
+            
+            
     
     return render_template("signup.html")
 
