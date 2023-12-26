e0b55cc84d070419750b46fddc41268caa80bd88
Zachary Dj <zachary@zacharys-mbp.home>
2023-12-25 23:41:03 -0500

    fixed bug with login system

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,30 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db 
 from .models import User
-from flask_login import login_user, logout_user, login_required
-from werkzeug.security import
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login",methods=['GET','POST'])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+        
+        user_exists = User.query.filter_by(email=email).first()
+        if user_exists:
+            if check_password_hash(user_exists.password, password):
+                flash("Logged in", category="success")
+                #stores what user the person is signed in as in the session
+                login_user(user_exists, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash("Password is incorrect.", category='error')
+        else:
+            flash("Email does not exist", category='error')
+        
     return render_template("login.html")
 
 @auth.route("/sign-up", methods=['GET','POST'])
@@ -22,7 +36,7 @@ def sign_up():
         password2 = request.form.get("password2")
         
         #filter database to check is email=email
-        email_exists = User.query.fliter_by(email=email).first() 
+        email_exists = User.query.filter_by(email=email).first() 
         username_exists = User.query.filter_by(username=username).first()
         if email_exists:
             flash("Email is already in use", category='error')
@@ -38,15 +52,20 @@ def sign_up():
             flash("Email is invalid", category='error')
         else:
             #if user account is valid then add user to database
-            new_user = User(email=email, username=username, password=password1)
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
+            #login user after creating account
+            login_user(new_user,remember=True)
             flash("Account created!")
             return redirect(url_for('views.home'))
                
     return render_template("signup.html")
 
+
 @auth.route("/logout")
+@login_required #only possible to log out if you're signed out
 def log_out():
+    logout_user()
     #return to home page
     return redirect(url_for("views.home"))
