ed43365ef3775c25cf829ab2786f6323a61f564c
Zachary Dj <zachary@zacharys-mbp.home>
2023-12-26 14:50:37 -0500

    added redirect to home page after creating post

--- website/models.py
+++ website/models.py
@@ -9,11 +9,14 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())#date of account creation
+    posts = db.relationship("Post", backref="user", passive_deletes=True)
+    #reference all posts that a user has, backref: access posts with Post.user, passive_deletes: allows all posts of user to be deleted
     
  #model for posts
- class Post(db.model):
+ 
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text =  db.Column(db.Text, nullable=False)#must have some text
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())#date of account creation
-    author = db.Column(db.Integer, db.ForeignKey("user.id"))
-        
+    author = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False) #accessing User.id
+        #CASCADE: delete all posts user has when his account is deleted
